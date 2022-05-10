4462c2b8b77ddce2341788f687983702e373b840
Francois D'Agostini <dagostini.francois@bcg.com>
2022-05-10 19:25:08 -0400

    addition of an API path

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var bodyParser = require("body-parser");
+var cors = require('cors');
 
 var express = require('express');
 var app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
@@ -33,6 +35,21 @@ app.post("/removetask", function (req, res) {
     }
     res.redirect("/");
 });
+
+app.get('/api/messages', (req, res) => {
+    return res.send(Object.values(messages));
+  });
+
+let messages = [
+{id: 1, title: "a message", description: "a simple description"},   
+{id: 2, title: "this is a title", description: "and this is a description"},   
+{id: 3, title: "titles are good", description: "but descriptions are better"},   
+{id: 4, title: "hidden subject", description: "and a hidden message"},   
+{id: 5, title: "How to become rich", description: "Exploit your skill as a self-employed expert and invest in it."},   
+{id: 6, title: "How to become famous", description: "Know your brand"},   
+{id: 7, title: "How to become attractive", description: "Give some compliments"},   
+{id: 8, title: "All you need is love", description: "There's nothin' you can do that can't be done"},   
+];
 
 app.listen(8080, function () {
     console.log('Running on port 8080!')
